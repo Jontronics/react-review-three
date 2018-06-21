512a4edb4efc4671d31233c7cfb5649072f84e78
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-06-21 13:02:47 -0400

    finished 4,5,6 conditinoal rendering is awesome wooo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation'
+import Char from './Char/Char'
 
 class App extends Component {
   
@@ -8,12 +9,27 @@ class App extends Component {
     userInput: ''
   }
   
-  inputChangeHandler = (event) => {
+  inputChangedHandler = (event) => {
     this.setState({userInput: event.target.value});
+  }
+  
+  deleteCharHandler = ( index ) => {
+    const text = this.state.userInput.split('');
+    text.splice(index,1)
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
   }
   
   
   render() {
+    
+    const charList = this.state.userInput.split('').map((ch, index) => {
+        return <Char 
+          character={ch} 
+          key={index}
+          clicked={() => this.deleteCharHandler(index)} />;
+      });
+    
     return (
       <div className="App">
         <h1>This is a React JS Review #3</h1>
@@ -29,10 +45,11 @@ class App extends Component {
         <hr />
         
         <input type="text" 
-          onChange={this.inputChangeHandler}
+          onChange={this.inputChangedHandler}
           value={this.state.userInput}  />
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length}/>
+        {charList}
         
       </div>
     );
